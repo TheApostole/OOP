e39bafbf7367fcdd173e859d505edb6038a32a7a
TheApostole <yaloza99@mail.ru>
2023-01-12 22:44:09 +0300

    "Задание 3" исправил

--- src/Car.java
+++ src/Car.java
@@ -1,37 +1,41 @@
 public class Car {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
 
     public Car(String brand, String model, int year, String country, String color, double engineVolume) {
         this.brand = brand;
+        this.model = model;
+        this.country = country;
+        this.year = year;
+        this.color = color;
+        this.engineVolume = engineVolume;
+        checks();
+    }
+    public void checks(){
         if(brand == null || brand.trim().isEmpty()) {
             this.brand = "default";
         }
-        this.model = model;
         if (model == null || model.trim().isEmpty()){
             this.model = "default";
         }
-        this.country = country;
         if (country == null || country.trim().isEmpty()) {
             this.country = "default";
         }
-        this.year = year;
         if(year <= 0) {
             this.year = 2000;
         }
-        this.color = color;
         if (color == null || color.trim().isEmpty()) {
             this.color = "белый";
         }
-        this.engineVolume = engineVolume;
         if(engineVolume <= 0) {
             this.engineVolume = 1.5;
         }
     }
+
     @Override
     public String toString() {
         return "Марка: " + brand + ", модель: " + model + ", год выпуска: " + year
