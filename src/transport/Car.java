dfa2313d050f484684de45d6ac5430f5217700ca
TheApostole <yaloza99@mail.ru>
2023-01-23 04:58:47 +0300

    "ООП: Инкапсуляция", Задание 2 сделано

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -207,7 +207,5 @@ public class Car {
             }
             return " доступен";
         }
-
-
     }
 }
--- src/transport/Main.java
+++ src/transport/Main.java
@@ -1,5 +1,7 @@
 package transport;
+
 import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
 
